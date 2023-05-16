ab449f474584957730232b3f36ec61ec6059212c
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-05-16 14:33:46 -0400

    initializing repo

---
+++ Util.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace CatWorx.BadgeMaker
+{
+  class Util
+  {
+ public static void PrintEmployees(List<Employee> employees)
+  {
+   for (int i = 0; i < employees.Count; i++) 
+{
+  string template = "{0,-10}\t{1,-20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+}
+  }
+    public static void MakeCSV(List<Employee> employees)
+    {
+        // Check to see if folder exists
+if (!Directory.Exists("data")) 
+{
+  // If not, create it
+  Directory.CreateDirectory("data");
+}
+using (StreamWriter file = new StreamWriter("data/employees.csv"))
+{
+  // Any code that needs the StreamWriter would go in here
+}
+    }
+  }
+}
